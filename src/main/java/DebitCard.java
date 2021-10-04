2e0c50b795a7446b46025f81a311d25d8c810dd9
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-10-04 14:41:20 +0100

    debitCard charges and transaction fee passed

--- src/main/java/DebitCard.java
+++ src/main/java/DebitCard.java
@@ -1,2 +1,31 @@
-public class DebitCard {
+import java.util.ArrayList;
+
+public class DebitCard implements IChargeable {
+
+    private ArrayList<Double> charges;
+
+    public DebitCard() {
+        this.charges = new ArrayList<>();
+    }
+
+    public double getTransactionFee(double purchaseAmount) {
+        return purchaseAmount*0.02;
+    }
+
+    public void charge(double purchaseAmount) {
+        this.charges.add(purchaseAmount);
+
+    }
+
+    public ArrayList<Double> getCharges() {
+        return charges;
+    }
+
+    public void setCharges(ArrayList<Double> charges) {
+        this.charges = charges;
+    }
+
+    public int countCharges() {
+        return  this.charges.size();
+    }
 }
--- src/test/java/DebitCardTest.java
+++ src/test/java/DebitCardTest.java
@@ -1,2 +1,26 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DebitCardTest {
+
+    private DebitCard debitCard;
+
+    @Before
+    public void before(){
+        debitCard = new DebitCard();
+    }
+
+    @Test
+    public void hasTransactionFee(){
+        assertEquals(1,debitCard.getTransactionFee(50),0.001);
+    }
+
+    @Test
+    public void hasCharges(){
+        debitCard.charge(20);
+        assertEquals(1,debitCard.countCharges());
+    }
 }
